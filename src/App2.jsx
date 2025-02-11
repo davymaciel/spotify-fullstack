618360e932d05859f1b3325b49da0c724d48b148
davymaciel <davymaciel700@gmail.com>
2025-02-11 18:23:46 -0300

    organização do ambiete e criação do header

---
+++ src/App2.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App2 = () => {
+  return (
+    <div>App2</div>
+  )
+};
+
+export default App2
+
+// rafce
+
+// Arrow Function
+// const App2 = () => <h1>Olá, Mundo!</h1>;
